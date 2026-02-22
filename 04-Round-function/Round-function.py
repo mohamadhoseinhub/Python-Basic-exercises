e9b7783f446b49ec7ee00f338a88fd843adbec06
mohamadhosein <mohamadhoseinmoshtaghi1400@gmail.com>
2026-02-22 23:43:02 +0330

    add round function

---
+++ 04-Round-function/Round-function.py
@@ -0,0 +1,26 @@
+# تابعی برای رند کردن اعداد
+
+x = 3.1561
+
+rn = round(x)   # خروجی میشود 3
+
+rn2 = round(x , 2)  # خروجی میشود 3.15
+
+rn3 = round(x , 3)  # خروجی میشود 3.156
+
+# این تابع کاربرد های زیادی دارد برای مثال
+
+'مثال'
+
+i = float (input('pls input a number for shoaa circle >>'))
+
+adad_py = 3.141592653589793238
+
+py_r = round(adad_py , 2)
+
+print ('s >>' , i*i*py_r)
+
+
+
+
+
