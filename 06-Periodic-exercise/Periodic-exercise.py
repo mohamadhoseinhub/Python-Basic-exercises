0e0eca09c5b51947b445ff10831ef9b1b286834f
mohamadhosein <mohamadhoseinmoshtaghi1400@gmail.com>
2026-02-26 01:57:34 +0330

    add periodic exercises

---
+++ 06-Periodic-exercise/Periodic-exercise.py
@@ -0,0 +1,19 @@
+# ============================
+# Mini Project 1
+# Topics Used:
+# - Data type
+# - Type conversion
+# - Operators
+# - input function
+# - round function
+# - string indexing
+# ============================
+
+
+
+
+
+
+
+
+
