28539efe8414b4893995d44355ad10318d6e768e
mohamadhosein <mohamadhoseinmoshtaghi1400@gmail.com>
2026-02-26 01:59:20 +0330

    add find error ex ..

--- 06-Periodic-exercise/Periodic-exercise.py
+++ 06-Periodic-exercise/Periodic-exercise.py
@@ -20,6 +20,51 @@ print (str(z) * 10)  # String multiplication
 
 
 
+# --Find error exercises--
+
+'''      
+x = 'ali # Syntax error (')
+y = 2
+print (type(y))
+'''
+
+'''
+s.5 = 10  # Syntax error (variable name rules) special character
+print (type (s.5))
+'''
+
+'''   # Syntax error (variable rules)
+1.    1number = 5  
+
+2.    my-number = 10
+
+3.    for = 20
+
+4.    my number = 30
+
+5.    @variable = 40
+
+6.    float! = 50
+
+7.    #score = 60
+
+8.    123abc = 70
+
+9.    class = 80
+
+10.total sum = 90
+'''
+
+'''
+t = 10
+t = y + t   # Because the variable y was used before it was defined.
+y = 5
+print (t)
+'''
+
+
+
+
 
 
 
