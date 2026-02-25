27e7d4c3adc6ff1a78029d0a7450cef7721c9ee8
mohamadhosein <mohamadhoseinmoshtaghi1400@gmail.com>
2026-02-25 23:32:11 +0330

    add type exe...

--- 01-Data-type/02-Data-type-exercises.py
+++ 01-Data-type/02-Data-type-exercises.py
@@ -11,24 +11,18 @@ name = 'mohamad hosein'
 is_student = True 
 print ('danesh amoz' , is_student)
 
+# Type function exercises
 
+xtint = 123
 
+print (type(xtint))
 
+# Type casting exercises
 
+intdata = 56 
+floatdata = 25.33
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print (int (floatdata))
+print (float(intdata))
+print (bool (intdata))
 
